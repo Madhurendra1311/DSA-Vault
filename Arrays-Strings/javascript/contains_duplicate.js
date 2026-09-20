7f53e7048f25091be28c2d45e8ede4e64cf88696
Madhurendra1311 <kmadhu1311@gmail.com>
2026-09-21 00:06:38 +0530

    Add contains duplicates implementation

--- Arrays-Strings/javascript/contains_duplicate.js
+++ Arrays-Strings/javascript/contains_duplicate.js
@@ -3,3 +3,51 @@
 var containsDuplicate = function (nums) {
     return new Set(nums).size !== nums.length;
 };
+
+// Approach 2:
+
+var containsDuplicate = function (nums) {
+    hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (hash[nums[i]]) {
+            return true
+        } else {
+            hash[nums[i]] = 1
+        }
+    }
+    return false
+};
+
+// Approach 3
+
+var containsDuplicate = function (nums) {
+    hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (hash[nums[i]]) {
+            hash[nums[i]] += 1
+        } else {
+            hash[nums[i]] = 1
+        }
+    }
+    values = Object.values(hash)
+    for (let j = 0; j < values.length; j++) {
+        if (values[j] > 1) {
+            return true
+        }
+    }
+    return false
+};
+
+// Approach 4:
+var containsDuplicate = function (nums) {
+    const seen = new Set();
+
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true; // Duplicate found early, exit immediately
+        }
+        seen.add(num);
+    }
+
+    return false; // All elements are unique
+};
